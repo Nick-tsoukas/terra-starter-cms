6c1f59161781abcc82371f6ed1fb414b77ab116c
Nick Tsoukas <nick.tsoukas101@gmail.com>
2019-10-18 17:41:54 -0500

    .lllll";;

---
+++ src/templates/video-post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { kebabCase } from 'lodash'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+const VideoPost = (props) => {
+    return (
+        <div>Hello this is the start of a blog post</div>
+    )
+}
+
+export default VideoPost;
